8ab9970fa5baab512a2e5fcb93a18825b786fb7a
Dylan Tasat <dantasat11@hotmail.com>
2017-10-24 16:12:56 -0300

    Finished books page with filters working

--- src/app/main.js
+++ src/app/main.js
@@ -1,14 +1,34 @@
 (function(){
     const app = angular.module('wbooks',[]);
 
-    app.controller('FilterController',function(){
+    app.factory('booksService',['$http',function($http){
+        this.books = [];
+        this.getBooks = function(booksService){
+            $http.get('assets/Books.json').then(function(data){
+                booksService.books = data.data;
+            },null);
+        }
+        this.sortBooks = function(booksService,order){
+            if (order === '2'){
+                booksService.books.sort((a,b) => a.title > b.title);
+            }else if (order === '3'){
+                booksService.books.sort((a,b) => a.author > b.author);
+            }
+        }
+        return this;
+    }]);
+
+    app.controller('FilterController',['booksService', function(booksService){
         this.filters = [
             {name: 'Seleccionar filtro', disabled: true, value: '1'},
             {name: 'Nombre', disabled: false, value: '2'},
             {name: 'Autor', disabled: false, value: '3'}
         ];
         this.actualFilter = this.filters[0].value;
-    });
+        this.changeFilter = function(filter, main){
+            booksService.sortBooks(booksService, filter.actualFilter);
+        };
+    }]);
 
     app.controller('SearchController',function(){
         this.text = '';
@@ -18,23 +38,11 @@
         }
     });
 
-    app.controller('BooksController',['$http', function($http){
-        this.books = [];
-        const act = this;
-        $http.get('Books.json').then(
-            function(data){
-                act.books = data.data
-                act.books.map(function(item){
-                    if (item['image_url'] === null){
-                        item['image_url'] = '/assets/default_book.svg';
-                    }
-                    item['show'] = true;
-                });
-            },
-            function(data){
-                console.log(data);
-            }
-        );
+    app.controller('BooksController',['booksService', function(booksService){
+        booksService.getBooks(booksService);
+        this.getBooks = function(){
+            return booksService.books;
+        }
     }]);
 
 })();
